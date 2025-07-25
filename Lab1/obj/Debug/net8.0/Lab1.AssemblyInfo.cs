818530d1f63d53aab246b69e6cc79b9464b472ea
AMAR4190 <benamar4190@gmail.com>
2025-07-25 20:26:56 +0000

    Lab 4 updated

--- Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net8.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f33f68713c174c7e4af24a43b1b997467e2cab04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccc19773de5150e59ff03e92ea6d25b8f127781c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -15,35 +15,25 @@ class Car
         Console.WriteLine("Year: " + year);
     }
 
-    // Method: Start()
-    // This method simulates the car starting and prints a message.
     public void Start()
     {
         Console.WriteLine("The car is starting.");
     }
 
-    // Method: Drive(int miles)
-    // This method simulates the car driving a certain number of miles
-    // and prints a message including the miles driven.
     public void Drive(int miles)
     {
-        Console.WriteLine($"The car drove {miles} miles.");
+        Console.WriteLine("The car drove " + miles + " miles.");
     }
 
-    // Method: GetDescription()
-    // This method returns a string combining the car's year, color, and model.
     public string GetDescription()
     {
-        // Using string interpolation for a clean and readable way to combine the fields.
-        return $"{year} {color} {model}";
+        return year + " " + color + " " + model;
     }
 
-    // New method: Repaint(string newColor)
-    // This method updates the car's color and prints a confirmation message.
     public void Repaint(string newColor)
     {
-        this.color = newColor; // 'this.color' refers to the 'color' field of the current object
-        Console.WriteLine($"The car has been repainted to {newColor}.");
+        this.color = newColor;
+        Console.WriteLine("The car has been repainted to " + newColor + ".");
     }
 }
 
@@ -54,23 +44,19 @@ class Program
         // Creating a Car object and assigning values to its properties
         Car myCar = new Car();
 
-        // Assigning initial values to the attributes of the 'myCar' object.
         myCar.model = "Civic";
         myCar.color = "Black";
-        myCar.year = 2020;
+        myCar.year = 2020; // Calling the Display method
 
-        myCar.Display();           // Calling the Display method
-        myCar.Start();             // Calling the Start method
-        myCar.Drive(50);           // Calling the Drive method
+        myCar.Display();
+        myCar.Start();
+        myCar.Drive(50);
 
-        // Calling the GetDescription() method, storing the result, and printing it.
         string carDescription = myCar.GetDescription();
-        Console.WriteLine($"Car Description: {carDescription}");
+        Console.WriteLine("Car Description: " + carDescription);
 
-        // Calling the new Repaint() method to change the car's color to "red".
         myCar.Repaint("red");
 
-        // Confirming that the color field has been updated by printing it directly.
-        Console.WriteLine($"Current car color (after repaint): {myCar.color}");
+        Console.WriteLine("Current car color (after repaint): " + myCar.color);
     }
 }
